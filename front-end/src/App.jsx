21100dbc49414f04a575d566efc7245c547ab9cb
ArthurPB25 <arthurprimobrandao@gmail.com>
2025-09-10 22:12:00 -0300

    Criação da aplicação e header

---
+++ front-end/src/App.jsx
@@ -0,0 +1,27 @@
+import Header from "./components/Header"
+import Item from "./components/Item";
+
+function App() {
+  
+  
+    return (
+    <>
+      <Header/>;
+      <section>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(225px,1fr))] mx-auto  max-w-7xl  p-8">
+         <Item/>;
+         <Item/>;
+         <Item/>;
+         <Item/>;
+         <Item/>;
+         <Item/>;
+         <Item/>;
+        </div>
+      </section>
+     
+    </>
+)
+  
+}
+
+export default App
